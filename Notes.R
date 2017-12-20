cd303e4c6c723ef00a32425fde81e06951f3efeb
shoganhennessy <senan.hoganhennessy@pomona.edu>
2017-12-19 16:44:31 -0800

    commit.  Sweave file won't accept bibliography.bib

---
+++ Notes.R
@@ -0,0 +1,13 @@
+# Paper: stylised model for mobility:  The Haves and the Have-Nots.
+# Note the idea in the paper may be extended to more groups than two, run simulations on this.
+
+
+# Papers to look over:
+  
+# https://eml.berkeley.edu//~moretti/mao.pdf
+
+# http://ftp.iza.org/dp7514.pdf 
+
+# https://web.stanford.edu/group/scspi/_media/working_papers/solon_theoretical-models.pdf
+
+# http://darp.lse.ac.uk/PapersDB/Becker-Tomes_(JPolE_79).pdf 
