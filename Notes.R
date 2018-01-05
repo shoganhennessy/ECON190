42ba715ca0880990227fcd6c13a363a4d6bb3bc7
shoganhennessy <31498943+shoganhennessy@users.noreply.github.com>
2018-01-05 14:56:57 -0800

    JMP 1993

--- Notes.R
+++ Notes.R
@@ -10,9 +10,14 @@
 # Importantly: are there haves and have-not split?
 
 # Idea 2. Paper that takes OLS residuals as unobserved skill.
+# john murphy pierce 93
+# Orignal paper
+# https://web.stanford.edu/group/scspi/_media/pdf/Classic_Media/Juhn_Murphy_Pierce_1993_Income%20and%20Wealth.pdf
 # Run again, with CPS and other data set.
 # Run with better predictive models.
 # Does increased variance in error terms go away?  If so, attribute to over-reliance on OLD framework.
+
+
 
 # Papers to look over:
   
