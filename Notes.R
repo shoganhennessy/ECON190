f253ddfeb5cd5543ffe7df53343176d7a7bf0b76
shoganhennessy <31498943+shoganhennessy@users.noreply.github.com>
2018-01-22 23:36:21 -0800

    Add files via upload

---
+++ Notes.R
@@ -0,0 +1,24 @@
+# Title:
+# The Rise in Returns to Skill?
+# A Modern Regression Analysis of Wage Inequality in the Current Population Survey
+
+# Abstract:
+# Wage Inequality has been documented as rising for many years in the economic literature, yet evidence that attributes this to a rise in return to skill use out-dated and orthodox regression approach to predicting wages.  Multiple significant studies decompose wage inequality in order to attribute a large portion of the rise in inequality to result from a rise in returns to skill, including most notably John, Murphy, Pierce (1993).  The statistical analysis of these studies rely heavily on the approach of applying Ordinary Least Squares regression algorithm to the Mincer earnings function, which dates back to 1954.  This study decomposes the rise in wage inequality for 1968-2015 in the March CPS using newer regression approaches, including random forests and artificial neural network algorithms.  These machine learning approach more accurately predict wages and more effectively simulate the vast complexities of an open labour market, providing a more robust analysis of the returns to skill in the US over 1968-2015. 
+
+
+
+# JMP 1993
+# https://web.stanford.edu/group/scspi/_media/pdf/Classic_Media/Juhn_Murphy_Pierce_1993_Income%20and%20Wealth.pdf
+# Does increased variance in error terms go away?  If so, attribute to over-reliance on OLD framework.
+
+
+
+# Papers to look over:
+  
+# https://eml.berkeley.edu//~moretti/mao.pdf
+
+# http://ftp.iza.org/dp7514.pdf 
+
+# https://web.stanford.edu/group/scspi/_media/working_papers/solon_theoretical-models.pdf
+
+# http://darp.lse.ac.uk/PapersDB/Becker-Tomes_(JPolE_79).pdf 
