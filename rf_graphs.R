81cbf67a9ca684d7b5112d3336a983fe5408e36d
Senan Hogan-H <31498943+shoganhennessy@users.noreply.github.com>
2018-04-02 22:30:36 -0700

    Update rf_graphs.R

--- rf_graphs.R
+++ rf_graphs.R
@@ -1,12 +1,13 @@
 # Random Forest Variable Importance, by hourly wage
+CPS.data <- fread('CPS_data.csv', header = T, sep = ',')
 set.seed(47)
 
 years <- c(c(1980:2007), c(2009:2016))
-year <- r_squared <- 
-  education_importance <- age_importance <- female_importance <- c()
+year <- r_squared_forest <- 
+  education_importance <- age_importance <- female_importance <- race_importance <- c()
 
-log_wage_equation <- log(rhrwage) ~ age + female + race + citizen + 
-  married + rural + suburb + centcity + selfemp + unmem + education
+log_wage_equation <- log(rhrwage) ~ age + female + race + 
+  married + rural + suburb + centcity + selfemp + education
 
 for (i in years){
   print(i)
@@ -21,7 +22,8 @@ for (i in years){
           na.action = na.pass, importance = T,
           metric='RMSE')
   
-  r_squared_forest <- (r_squared_forest, )
+  r_squared_forest <- c(r_squared_forest, 
+                       as.numeric(CPS_forest.reg$results[2][1]))
   
   CPS_forest.Imp <- varImp(CPS_forest.reg, scale = FALSE)
   
@@ -31,6 +33,8 @@ for (i in years){
     c(age_importance, CPS_forest.Imp$importance$Overall[1])
   female_importance <- 
     c(female_importance, CPS_forest.Imp$importance$Overall[2])
+  race_importance <- 
+    c(race_importance, CPS_forest.Imp$importance$Overall[3])
 }
 data_frame(year, education_importance, 
            age_importance, female_importance) %>% 
@@ -47,7 +51,52 @@ data_frame(year, education_importance,
                  colour = 'Gender')) +
   geom_smooth(aes(y = female_importance, 
                   colour = 'Gender'), method='lm') +
+  geom_point(aes(y = race_importance, 
+                 colour = 'Race')) +
+  geom_smooth(aes(y = race_importance, 
+                  colour = 'Race'), method='lm')
   labs(x= 'Year', y='Variable Importance', colour = '') +
   theme_classic()  + 
   theme(legend.position='bottom')
-ggsave('importance_graph4.png')
+ggsave('importance_graph.png')
+
+
+
+
+
+
+
+# Comparing R-squared values.
+years <- c(c(1980:2007), c(2009:2016))
+year <- r_squared_mincer <- r_squared_mincer_adj <- c()
+for (i in years){
+  print(i)
+  year <- c(year, i)
+  
+  CPS_mincer.reg <- CPS.data %>% subset(year==i) %>%
+    lm( log(rhrwage) ~ education + I(age-education-6) + I((age-education-6)^2), 
+        data=.)
+  
+  r_squared_mincer <- c(r_squared_mincer, summary(CPS_mincer.reg)$r.squared)
+  
+  CPS_mincer_adj.reg <- CPS.data %>% subset(year==i) %>%
+    lm( log(rhrwage) ~ I(education) + I(education^2) +
+          I(age-education-6) + I((age-education-6)^2) +
+          I((age-education-6)^3) + I((age-education-6)^4), 
+        data=.)
+  
+  r_squared_mincer_adj <- c(r_squared_mincer_adj, summary(CPS_mincer_adj.reg)$r.squared)
+  
+}
+data_frame(year, r_squared_mincer, r_squared_mincer_adj, r_squared_forest) %>% 
+  ggplot(aes(x = year)) +
+  geom_point(aes(y = r_squared_mincer, colour = 'Mincer')) +
+  geom_point(aes(y = r_squared_mincer_adj, colour = 'Mincer, adjusted')) +
+  geom_point(aes(y = r_squared_forest, colour = 'Random forest'))  +
+  labs(x= 'Year', y='R Squared', 
+       colour = '') +
+  theme_bw() +
+  theme(legend.position=c( 0.8375, 0.2), legend.background=element_blank()) +
+  scale_x_continuous(breaks=seq(1980,2016,5))
+ggsave('rsquared.png')
+
